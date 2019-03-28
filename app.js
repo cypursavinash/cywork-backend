34dbb07c4865186777a5b1e96634cc3dd176c143
avinash singh <avinash.cypurs@gmail.com>
2019-03-28 19:37:00 +0530

    database

--- app.js
+++ app.js
@@ -3,16 +3,39 @@ var app = express();
 var PORT = 5001;
 var dataSet = []
 var cors = require('cors');
-
+var mongodb = require('mongodb').MongoClient;
 dataSet.push({ name: "user"});
+var dbName = 'registeruser'
+var mongodbUrl = 'mongodb://localhost/'; 
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/register',function(req,res){
+
+
+
+app.post('/register', async function(req,res){
     let userRegistrationData = req.body;
     console.log(userRegistrationData);
-    
+    await mongodb.connect(mongodbUrl, async (err, db)=>{
+        if(err){
+            console.log("Error in connection")
+            console.log(err)
+        };
+        var polDb  = await db.db(dbName);
+
+         await polDb.collection('polls').insertOne(userRegistrationData,(err,res)=>{
+            if(err){
+                console.log("Error in inserting data");
+                console.log(err);
+            };
+            console.log("1 document inserted");
+            db.close();
+         })
+
+
+
+    })
     res.json(userRegistrationData);
 })
 
